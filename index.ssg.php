3657ed80c458934f09ee33b1b27a5ad5d73877f5
soyeonlee57 <soyeonlee57@gmail.com>
2021-07-27 17:55:19 +0900

    php_simple

--- index.ssg.php
+++ index.ssg.php
@@ -1,12 +1,2 @@
-<?php
-require_once "data.php";
-require_once "head.php";
-
-$articles = &getArticles();
-?>
-
-안녕
-
-<?php
-require_once "foot.php";
-?>
+<?php require_once "head.php"; ?>
+메인
--- pf.ssg.php
+++ pf.ssg.php
@@ -1,10 +1,2 @@
-<?php
-require_once "data.php";
-require_once "head.php";
-?>
-
+<?php require_once "head.php"; ?>
 포트폴리오
-
-<?php
-require_once "foot.php";
-?>
